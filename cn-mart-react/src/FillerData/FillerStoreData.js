f2d9fe1eb7a1c3152bb21794b9566f1803cd8ad0
Cheng Her <ckher96@gmail.com>
2023-11-03 14:44:02 -0500

    add filler data to store page

---
+++ cn-mart-react/src/FillerData/FillerStoreData.js
@@ -0,0 +1,7 @@
+const fillerData = [
+  { name: "Hot Cheetos", price: 10, cat: "chip" },
+  { name: "Fanta", price: 10, cat: "soda" },
+  { name: "ramen", price: 10, cat: "noodle" },
+  { name: "Air head", price: 10, cat: "candy" },
+];
+export default fillerData;
--- cn-mart-react/src/components/Store/Store.js
+++ cn-mart-react/src/components/Store/Store.js
@@ -1,8 +1,12 @@
-import style from "./Store.module.css"
+import style from "./Store.module.css";
+import fillerData from "../../FillerData/FillerStoreData";
 
 export default function Store() {
+  const formatedItems = fillerData.map((item) => <p>{item.name}</p>);
+
   return (
     <>
+      {formatedItems}
       <h2>What are you looking for?</h2>
       <ul className={style.container}>
         <li>Chips</li>
