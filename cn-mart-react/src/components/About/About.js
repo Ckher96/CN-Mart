a2431459a9b274897f5c4a963d9fb7ef70332242
lher14 <136016199+lher14@users.noreply.github.com>
2023-11-09 13:30:18 -0600

    add about page content

--- cn-mart-react/src/components/About/About.js
+++ cn-mart-react/src/components/About/About.js
@@ -6,12 +6,16 @@ function About() {
 
     return (
         <div>
-            <h1 className={style.AboutH1}>About</h1>
-        <p>{p}</p>
-        <input value={p} onChange={(e) => {
-            setP(e.target.value)
-        }} />
+            <h1>About Us</h1>
+            <p>Ea labore ut mollit deserunt officia dolor minim quis. Laborum labore officia laboris culpa consectetur est sunt dolore est mollit in excepteur amet commodo. Veniam commodo occaecat proident eiusmod mollit cillum cillum incididunt excepteur et anim laborum. Cillum do elit laboris tempor amet ea quis duis elit voluptate.
+
+Lorem proident non exercitation sint Lorem ut labore ipsum non pariatur excepteur exercitation. Id veniam qui ea aliqua reprehenderit duis amet eu pariatur est non. Cillum occaecat deserunt est reprehenderit occaecat anim duis exercitation nisi duis labore. Culpa proident quis id fugiat qui. Non aliquip culpa ullamco esse reprehenderit nostrud est enim.
+
+Officia ut quis magna consequat consequat et adipisicing sint. Sit in et cupidatat commodo magna non id. Dolore voluptate dolore laboris veniam. Consectetur est nostrud ad magna. Sunt sunt minim officia enim occaecat ullamco mollit non pariatur et culpa cillum est. Ut sunt velit voluptate Lorem eiusmod magna tempor nulla incididunt id quis anim ea.
+
+Eu excepteur esse ipsum nisi Lorem ipsum fugiat culpa Lorem minim adipisicing. Eiusmod deserunt in nulla cupidatat ex pariatur laborum. Reprehenderit incididunt aliqua voluptate exercitation sint ullamco sunt nostrud. Minim nostrud voluptate fugiat commodo nulla anim dolor pariatur qui exercitation ullamco do qui.</p>
         </div>
+        
         
     )
 }
