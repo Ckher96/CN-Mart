d1403d00122f64e7796ccd113c91d9fc8f958570
Cheng Her <ckher96@gmail.com>
2023-11-16 14:38:44 -0600

    add custom hook useFetch

---
+++ cn-mart-react/src/components/Utility/UseFetch.js
@@ -0,0 +1,34 @@
+import { useEffect, useState } from "react";
+
+export default function useFetch(url, method = "GET", body = null) {
+  const [data, setData] = useState([]);
+  const [err, setErr] = useState(null);
+
+  useEffect(() => {
+    async function getData() {
+      const options = {
+        method: method,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      };
+
+      if (method === "POST") {
+        options.body = body;
+      }
+
+      try {
+        const data = await fetch(url, options);
+        const formattedData = await data.json();
+        if (formattedData.data) setData(formattedData.data);
+        else throw new Error(formattedData.err);
+      } catch (e) {
+        setErr(e.message);
+      }
+    }
+    setErr(null);
+    getData();
+  }, [url, method, body]);
+
+  return { data, err };
+}
