284853f281fc55f7149bdf786a7d643789573755
Cheng Her <ckher96@gmail.com>
2023-11-17 14:22:51 -0600

    add addItem and removeItem to itemCard component and make itemCard dynamic to url

--- cn-mart-react/src/components/Utility/ItemCard.js
+++ cn-mart-react/src/components/Utility/ItemCard.js
@@ -1,20 +1,59 @@
-import { useContext } from 'react'
-import style from './ItemCard.module.css'
-import { CartContext } from '../../Context/CartContext'
+import { useContext } from "react";
+import style from "./ItemCard.module.css";
+import { CartContext } from "../../Context/CartContext";
+import { useLocation } from "react-router-dom";
 
-export default function ItemCard({item}) {
-    const {setCart} = useContext(CartContext)
+export default function ItemCard({ item }) {
+  const { cart, setCart } = useContext(CartContext);
+  const { pathname } = useLocation();
 
-    const clickHandler = () => {
-        setCart((p) => [...p, item])
+  const addHandler = () => {
+    if (cart.includes(item)) {
+      item.amount = item.amount + 1;
+      const newArray = cart.map((cartItem) => {
+        if (cartItem.name === item.name) {
+          cartItem.amount++;
+          return cartItem;
+        } else return cartItem;
+      });
+      setCart(newArray);
+    } else {
+      item.amount = 1;
+      setCart([...cart, item]);
     }
+  };
 
-    return (
-        <div className={style.card}>
-            <h3>{item.name}</h3>
-            <div>img</div>
-            <h4>{item.price}</h4>
-            <button onClick={clickHandler}>Add To Cart</button>
-        </div>
-    )
+  const removeHandler = () => {
+    const itemToBeRemoved = cart.find(
+      (cartItem) => item.name === cartItem.name
+    );
+    if (itemToBeRemoved.amount > 1) {
+      const newArray = cart.map((cartItem) => {
+        if (cartItem.name === item.name) {
+          cartItem.amount--;
+          return cartItem;
+        } else return cartItem;
+      });
+      setCart(newArray);
+    } else {
+        const newArray = cart.filter(
+          (currentItem) => item.name !== currentItem.name
+        );
+      setCart(newArray);
+    }
+  };
+
+  const buttonText = pathname !== "/cart" ? "Add To Cart" : "Remove From Cart";
+
+  return (
+    <div className={style.card}>
+      <h3>{item.name}</h3>
+      <div>img</div>
+      <div>{item.amount}</div>
+      <h4>{item.price}</h4>
+      <button onClick={pathname !== "/cart" ? addHandler : removeHandler}>
+        {buttonText}
+      </button>
+    </div>
+  );
 }
