1bfdf4d98c40229046f7316d1b57c580e8198f05
Cheng Her <ckher96@gmail.com>
2023-11-18 16:06:23 -0600

    add total price to cart component

--- cn-mart-react/src/components/Cart/Cart.js
+++ cn-mart-react/src/components/Cart/Cart.js
@@ -8,9 +8,12 @@ export default function Cart() {
 
   const formatedCart = cart.map(item => <ItemCard item={item} />)
 
+  const total = cart.reduce((acc, cartItem) => acc+=(cartItem.price * cartItem.amount), 0)
+
   return (
     <>
       <h1>Cart</h1>
+      <div>Total: {total}</div>
       <div className={style.cart_container}>
         {formatedCart}
       </div>
