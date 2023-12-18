136fdf3da1af91302ad3041b77d2ffb43f9c0729
Cheng Her <ckher96@gmail.com>
2023-12-18 15:42:01 -0600

    change cart context to match order model in backend

--- cn-mart-react/src/Context/CartContext.js
+++ cn-mart-react/src/Context/CartContext.js
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 export const CartContext = React.createContext();
 
 export default function CartContextProvider({ children }) {
-  const [cart, setCart] = useState([]);
+  const [cart, setCart] = useState({items: [], totalPrice:0});
   return (
     <CartContext.Provider value={{ cart, setCart }}>
       {children}
