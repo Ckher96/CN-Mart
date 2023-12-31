4fff35f4eb550dd57554211cdf94ad317ae79114
Cheng Her <ckher96@gmail.com>
2023-12-30 21:51:17 -0600

    add content to footer component

--- cn-mart-react/src/components/Footer/Footer.js
+++ cn-mart-react/src/components/Footer/Footer.js
@@ -1,5 +1,16 @@
+import style from "./Footer.module.css"
+
 export default function Footer() {
-    return (
-        <h1>Footer</h1>
-    )
+  return (
+    <div className={style.container}>
+      <div className={style.footerItem}>
+        <h3>Phone</h3>
+        <p>(000)000-0000</p>
+      </div>
+      <div className={style.footerItem}>
+        <h3>Email</h3>
+        <p>RandomEmail@email.com</p>
+      </div>
+    </div>
+  );
 }
