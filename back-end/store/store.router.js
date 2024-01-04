61c496506280126268c1aa3614d87ee02ed96781
Cheng Her <ckher96@gmail.com>
2024-01-04 13:53:55 -0600

    add StoreCat backend logic to pull from local mongodb

--- back-end/store/store.router.js
+++ back-end/store/store.router.js
@@ -1,18 +1,16 @@
 const router = require("express").Router();
 const Item = require("../models/Item")
 
-const { fillerData } = require("../FillerData/FillerData");
-
 router.route("/").get(async (req, res) => {
   const items = await Item.find()
   res.json({ data: items });
 });
 
-router.route("/:category").get((req, res) => {
+router.route("/:category").get(async (req, res) => {
   const { category } = req.params;
-  const filteredData = fillerData.filter((item) => item.cat === category);
-  if (!filteredData.length) res.json({ err: "no items" });
-  else res.json({ data: filteredData });
+  const categoryData = await Item.find({category: category})
+  if (!categoryData.length) res.json({ err: "no items" });
+  else res.json({ data: categoryData });
 });
 
 module.exports = router;
