c4518558e14cb471cfe84c65244195551e7418ff
Cheng Her <ckher96@gmail.com>
2024-01-07 00:38:07 -0600

    add dotenv package and change from a local database to an online database

--- back-end/server.js
+++ back-end/server.js
@@ -1,8 +1,10 @@
+require("dotenv").config()
+
 const app = require("./app");
 const mongoose = require("mongoose");
 
 app.listen(5000, () => {
   console.log("connected");
 });
-mongoose.connect("mongodb://localhost/appdb");
+mongoose.connect(process.env.DB);
 module.exports = app;
